1e36b27981ffd30e8474ed7fcd8ddc5530810a40
Petachek-Tom <tomsharikov@gmail.com>
2025-10-24 09:09:46 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "База знаний",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
